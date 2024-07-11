80ecd099a56a453e9601ac68196f3fb8f7374893
Jakub Matuszczak <kubamatuszczak2@gmail.com>
2024-07-12 00:44:41 +0200

    Update web.py

--- web.py
+++ web.py
@@ -6,11 +6,29 @@ import os
 
 st.title("Dodaj Tekst do Obrazu")
 
-# Wprowadzenie liczby obrazów
+
 howMany = st.number_input("Ile obrazów chcesz przesłać?", min_value=1, max_value=10, value=1, step=1)
 size = st.number_input("Rozmiar czcionki", min_value=10, max_value=100, value=30, step=1)
 
-# Listy do przechowywania danych wejściowych i przetworzonych obrazów
 uploaded_images = []
 texts = []
 output_images = []
+
+for i in range(howMany):
+    st.subheader(f"Obraz {i + 1}")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        image = st.file_uploader(f"Prześlij Obraz {i + 1}", type=["jpg", "jpeg", "png"], key=f"image_uploader_{i}")
+        uploaded_images.append(image)
+
+        text = st.text_area(f"Wprowadź tekst dla Obrazu {i + 1}", key=f"text_area_{i}")
+        texts.append(text)
+
+    with col2:
+        if uploaded_images[i] is not None and texts[i]:
+            output_image_path = f"output_image_{i + 1}.png"
+            image_with_text = add_centered_text_to_image(uploaded_images[i], texts[i], font_size=size, output_path=output_image_path)
+            output_images.append((output_image_path, image_with_text))
+            st.image(image_with_text, caption=f"Obraz Wyjściowy {i + 1}", use_column_width=True)
